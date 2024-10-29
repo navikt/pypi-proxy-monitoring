2ddaedf08966d28a1c68e6e6f735328a12fd9957
Erik Vattekar <erik.vattekar@nav.no>
2024-10-29 16:32:03 +0100

    test lookup on log insert id

--- cloud-function/main.py
+++ cloud-function/main.py
@@ -14,56 +14,29 @@ from google.cloud.bigquery import Client as BQClient
 
 logging.basicConfig(level=logging.INFO)
 
-class PythonPackage:
+def scan_package(package_name: str, package_version: str) -> Tuple[bool, str]:
+    package_and_version = f"{package_name}=={package_version}"
+    tmp = tempfile.NamedTemporaryFile()
+    with open(tmp.name, 'w') as f:
+        f.write(package_and_version)
 
-    def __init__(self, event_data: dict):
-        self.name, self.version, self.user_email = self._parse_event_data(event_data=event_data)
-        self.scan_report = ""
+    logging.info(f"Scanning {package_and_version}")
+    result = subprocess.run(["pip-audit", "-r", tmp.name, "-l", "--cache-dir", "/tmp"], capture_output=True)
+    if result.returncode != 0:
+        scan_report = f"{result.stderr.decode('utf-8')}\n{result.stdout.decode('utf-8')}"
+    else:
+        scan_report = result.stderr.decode('utf-8')
 
-    def _parse_event_data(self, event_data: dict) -> Tuple[str, str, str]:
-        name, version = self._extract_package_and_version_from_event(event_data=event_data)
-        user_email = self._extract_user_email_from_event(event_data=event_data)
+    logging.info(scan_report)
 
-        return name, version, user_email 
-
-    def _extract_package_and_version_from_event(self, event_data: dict) -> Tuple[str, str]:
-        request_url = event_data["protoPayload"]["request"]["requestUrl"]
-        request_url_parts = request_url.split("/")
-        package_name = request_url_parts[-2]
-        package_wheel_file = request_url_parts[-1]
-        package_version = self._extract_package_version_from_wheel_filename(package_wheel_file)
-
-        return package_name, package_version
-
-    def _extract_package_version_from_wheel_filename(self, wheel_file: str) -> str:
-        return wheel_file.split("-")[1]
-
-    def _extract_user_email_from_event(self, event_data: dict) -> str:
-        return event_data["protoPayload"]["authenticationInfo"]["principalEmail"]
-
-    def scan(self) -> bool:
-        package_and_version = f"{self.name}=={self.version}"
-        tmp = tempfile.NamedTemporaryFile()
-        with open(tmp.name, 'w') as f:
-            f.write(package_and_version)
-
-        logging.info(f"Scanning {package_and_version}")
-        result = subprocess.run(["pip-audit", "-r", tmp.name, "-l", "--cache-dir", "/tmp"], capture_output=True)
-        if result.returncode != 0:
-            self.scan_report = f"{result.stderr.decode('utf-8')}\n{result.stdout.decode('utf-8')}"
-            logging.error(self.scan_report)
-        else:
-            self.scan_report = result.stderr.decode('utf-8')
-            logging.info(self.scan_report)
-
-        return result.returncode != 0
+    return result.returncode != 0, scan_report
 
 
-def notify_user(package: PythonPackage) -> None:
+def notify_user(package_name: str, package_version: str, user_email: str, scan_report: str) -> None:
     slack_token = get_slack_token()
 
     client = WebClient(token=slack_token)
-    user = client.users_lookupByEmail(email=package.user_email)
+    user = client.users_lookupByEmail(email=user_email)
     if not user["ok"]:
         raise Exception("todo: user not found in slack throw exception")
 
@@ -71,7 +44,7 @@ def notify_user(package: PythonPackage) -> None:
     client.chat_postMessage(
         channel=user_id,
         text="test scan error :eyes:",
-        blocks=create_slack_message(package=package)
+        blocks=create_slack_message(package_name=package_name, package_version=package_version, scan_report=scan_report)
     )
 
 
@@ -88,49 +61,40 @@ def get_slack_token() -> str:
     return response.payload.data.decode("UTF-8")
 
 
-def create_slack_message(package: PythonPackage) -> list:
+def create_slack_message(package_name: str, package_version: str, scan_report: str) -> list:
     return [
         {
             "type": "section",
             "text": {
                 "type": "mrkdwn",
-                "text": f":warning: _*Sårbarhet oppdaget i pakke `{package.name}=={package.version}`*_",
+                "text": f":warning: _*Sårbarhet oppdaget i pakke `{package_name}=={package_version}`*_",
             }
         },
         {
             "type": "section",
             "text": {
                 "type": "mrkdwn", 
-                "text": f"```{package.scan_report}```",
+                "text": f"```{scan_report}```",
             }
         }
     ]
 
-
-def scan_package(event_data: dict) -> None:
-    python_package = PythonPackage(event_data=event_data)
-
-    vulnerability_discovered = python_package.scan()
-    if vulnerability_discovered:
-        notify_user(python_package)
-
-
 @functions_framework.cloud_event
 def entrypoint(cloud_event):
-   event_data = json.loads(base64.b64decode(cloud_event.data["message"]["data"]))
-   print("event:", cloud_event)
-   print("event_data", event_data)
-   insert_id = event_data["insertId"]
+    event_data = json.loads(base64.b64decode(cloud_event.data["message"]["data"]))
+    print("event:", cloud_event)
+    print("event_data", event_data)
+    insert_id = event_data["insertId"]
 
-   client = BQClient()
-   query = f"SELECT package, version, install_timestamp FROM `knada-dev.pypi_proxy_data.package_installations` WHERE log_insert_id = '{insert_id}'"
-   print("query", query)
-   df = client.query_and_wait(query).to_dataframe()
+    client = BQClient()
+    query = f"SELECT package, version, user_email FROM `knada-dev.pypi_proxy_data.package_installations` WHERE log_insert_id = '{insert_id}'"
+    res = client.query_and_wait(query)
 
-   print(df)
-   
-   #try:
-   #   scan_package(event_data=event_data)
-   #except Exception as e:
-   #   print("todo: post to nada-alerts")
-   #   raise
+    print("scanning package", f"{res[0]}=={res[1]}")
+    try:
+        has_vulnerability, scan_report = scan_package(res[0], res[1])
+        if has_vulnerability:
+            notify_user(res[0], res[1], res[2], scan_report=scan_report)
+    except Exception as e:
+        print("todo: post to nada-alerts")
+        raise
