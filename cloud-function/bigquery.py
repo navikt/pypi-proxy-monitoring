792f103c53c8d5da5f092f7f9e89c3caaf2a3b13
Erik Vattekar <erik.vattekar@nav.no>
2024-11-01 08:40:38 +0100

    add retry for reading package data, could occationally happen that the cloud function is triggered before the log entry is routed to bigquery

--- cloud-function/bigquery.py
+++ cloud-function/bigquery.py
@@ -2,17 +2,23 @@ import json
 from google.cloud.bigquery import Client
 from typing import Tuple
 from datetime import datetime
+from time import sleep
 
+MAX_NUM_READ_RETRIES = 3
 
 def fetch_unpacked_package_installation_info(log_insert_id: str) -> Tuple[str, str, str]:
     client = Client()
     query = f"SELECT package, version, user_email FROM `knada-dev.pypi_proxy_data.package_installations` WHERE log_insert_id = '{log_insert_id}'"
-    res = client.query_and_wait(query)
 
-    rows = [r for r in res]
-    if len(rows) == 1:
-        package_data = rows[0]
-        return package_data[0], package_data[1], package_data[2]
+    for i in range(MAX_NUM_READ_RETRIES):
+        res = client.query_and_wait(query)
+
+        rows = [r for r in res]
+        if len(rows) == 1:
+            package_data = rows[0]
+            return package_data[0], package_data[1], package_data[2]
+        
+        sleep(1)
 
     raise Exception(f"unable to read unpacked data for log_insert_id = '{log_insert_id}', length of view query results was {len(rows)}")
 
